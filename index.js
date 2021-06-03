c4aa94a4fa89b2332b0a11fe9bcca82f7a2e4cb6
Nick0808 <chowj2154@gmail.com>
2021-06-03 20:17:06 +0900

    Add Text File[index.html index.css index.js]

---
+++ index.js
@@ -0,0 +1,10 @@
+const ChoInfo = {
+    name:"Nick",
+    age:29,
+    gender:"Male",
+    isMarried:false,
+    favFood: [{name:"Kimchi", fatty:false}, {name:"Cheese burger", fatty:true}]
+
+}
+
+console.log(ChoInfo);
