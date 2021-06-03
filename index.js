7b383ff331c0b366805a8112b34baa8828da1fef
Nick0808 <chowj2154@gmail.com>
2021-06-03 22:38:33 +0900

    Add event[DOM]

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const ChoInfo = {
-    name:"Nick",
-    age:29,
-    gender:"Male",
-    isMarried:false,
-    favFood: [{name:"Kimchi", fatty:false}, {name:"Cheese burger", fatty:true}]
+// DOM(Document Object Module)
+title.innerHTML = "No more excuses!";
+console.error("100% error")
 
+const base_color = 'yellow';
+const change_color = 'red';
+
+function handleclick(){
+    const currentColor = title.style.color;
+    if(currentColor === base_color) {
+        title.style.color = change_color;
+    } else {
+        title.style.color = base_color;
+    }
 }
 
-console.log(ChoInfo);
+function init() {
+    title.style.color = base_color;
+    title.addEventListener("click", handleclick);
+}
+
+init();
+
+function handleOffline() {
+    alert("Bye Bye! See you next time");
+}
+
+function handleOnline(){
+    alert("Welcome to my page!");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
